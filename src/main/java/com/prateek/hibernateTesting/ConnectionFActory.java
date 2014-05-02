cc95cf7f7f998469ff33f0d6956763adac26c64c
Prateek <Prateekk@aditi.com>
2014-05-02 17:20:11 +0530

    more changes

--- src/main/java/com/prateek/hibernateTesting/ConnectionFActory.java
+++ src/main/java/com/prateek/hibernateTesting/ConnectionFActory.java
@@ -1,5 +1,12 @@
 package com.prateek.hibernateTesting;
 
+import org.hibernate.SessionFactory;
+
 public class ConnectionFActory {
+	
+	public SessionFactory getSession(){
+		
+		return new SeesionFactory();
+	}
 
 }
